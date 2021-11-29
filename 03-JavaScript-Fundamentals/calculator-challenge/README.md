fe12c02f511392659ad7472a388c42da2f0af487
amiehannah <64609830+amiehannah@users.noreply.github.com>
2021-11-29 11:52:20 +0000

    set DOM elements in JS

---
+++ 03-JavaScript-Fundamentals/calculator-challenge/README.md
@@ -0,0 +1,32 @@
+Task:
+To build a calculator using HTML, CSS and JavaScript. This calculator should accept inputs from a user, is able to perform a range of calculations and will produce an output.
+
+
+Breaking down the problem:
+Essential functionality needed -->
+Addition
+Subtraction
+Division
+Multiplication
+Delete/Clear
+Visually display the answer / output
+Also work with decimals
+
+
+Logic:
+
+
+UI:
+
+
+
+
+Requirements:
+- Minimum of 10 Git commits for the development.
+- Accept a minimum of 2 inputs, perform an operation and return the output.
+- Mobile first approach
+
+
+
+Approach:
+
---
+++ 03-JavaScript-Fundamentals/calculator-challenge/script.js
@@ -0,0 +1,30 @@
+// Getting DOM Elements
+let clear = document.querySelector("#clear");
+let plusMinus = document.querySelector("#plus-minus");
+let percentage = document.querySelector("#percentage");
+let divide = document.querySelector("#divide");
+let multiply = document.querySelector("#multiply");
+let subtract = document.querySelector("#subtract");
+let add = document.querySelector("#add");
+let decimal = document.querySelector("#decimal");
+let equals = document.querySelector("#equals");
+
+let calcBtnNine = document.querySelector("#nine");
+let calcBtnEight = document.querySelector("#eight");
+let calcBtnSeven = document.querySelector("#seven");
+let calcBtnSix = document.querySelector("#six");
+let calcBtnFive = document.querySelector("#five");
+let calcBtnFour = document.querySelector("#four");
+let calcBtnThree = document.querySelector("#three");
+let calcBtnTwo = document.querySelector("#two");
+let calcBtnOne = document.querySelector("#one");
+let calcBtnZero = document.querySelector("#zero");
+
+//would it be better to select all number buttons using querySelectorAll? 
+// document.querySelectorAll(".calculator__btn-num")
+
+
+// Variables
+
+
+// Functions
