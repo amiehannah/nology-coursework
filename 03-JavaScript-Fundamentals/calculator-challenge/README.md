e7ab424182940b3d1e3cf72f31ffb7c66a1edd31
amiehannah <64609830+amiehannah@users.noreply.github.com>
2022-01-01 11:38:26 +0000

    updated README.md

--- 03-JavaScript-Fundamentals/calculator-challenge/README.md
+++ 03-JavaScript-Fundamentals/calculator-challenge/README.md
@@ -8,17 +8,18 @@ Breaking down the problem --> Essential functionality needed:
 - Subtraction
 - Division
 - Multiplication
-- Delete/Clear
+- Clear/reset the display
 - Visually display the answer / output
 - Also work with decimals
 
 
 Logic:
-
-
-UI:
-
-
+1. Listens out for buttons pressed from a user and displays these
+2. Store this number to be used in calculations
+3. User then selects an operator button
+4. Listens for the next set of number buttons selected
+5. When the user clicks equals - this needs to perform the maths and return a total value
+6. When the user clicks clear the calculator needs to reset.
 
 
 Requirements:
@@ -29,4 +30,8 @@ Requirements:
 
 
 Approach:
+• First I started by selecting all the elements I needed to target in variables. I initially saved each number/operator into individual variables before changing my approach.
 
+• Instead by using querySelectorAll on numberBtns and operatorBtns (to select these elements) I realised I can later iterate over them, as this returns a node list and will be far more useful in some of the functionality.
+
+• The array iterator I used to iterate over the numberBtns/operatorBtns was forEach(). By adding an event listener, listening out for the click event 
--- 03-JavaScript-Fundamentals/calculator-challenge/script.js
+++ 03-JavaScript-Fundamentals/calculator-challenge/script.js
@@ -1,81 +1,116 @@
-// Getting DOM Elements
-let calcBtnClear = document.querySelector("#clear");
-let calcBtnPlusMinus = document.querySelector("#plus-minus");
-let calcBtnPercentage = document.querySelector("#percentage");
-let calcBtnDivide = document.querySelector("#divide");
-let calcBtnMultiply = document.querySelector("#multiply");
-let calcBtnSubtract = document.querySelector("#subtract");
-let calcBtnAdd = document.querySelector("#add");
-let calcBtnDecimal = document.querySelector("#decimal");
-let calcBtnEquals = document.querySelector("#equals");
+// -----------------------------------------------------------------------------
+// SELECTING DOM ELEMENTS
+// -----------------------------------------------------------------------------
+const clearBtn = document.querySelector("#clear");
+const equalsBtn = document.querySelector("#equals");
+const numberBtns = document.querySelectorAll(".calculator__btn-num");
+const operatorBtns = document.querySelectorAll(".calculator__btn");
+const calcDisplay = document.querySelector(".calculator__display");
+const topDisplay = document.querySelector(".storedValue");
 
-let calcBtnNine = document.querySelector("#nine");
-let calcBtnEight = document.querySelector("#eight");
-let calcBtnSeven = document.querySelector("#seven");
-let calcBtnSix = document.querySelector("#six");
-let calcBtnFive = document.querySelector("#five");
-let calcBtnFour = document.querySelector("#four");
-let calcBtnThree = document.querySelector("#three");
-let calcBtnTwo = document.querySelector("#two");
-let calcBtnOne = document.querySelector("#one");
-let calcBtnZero = document.querySelector("#zero");
+let initialValue = "";
+let storedValue = "";
+let sum = "";
 
-//would it be better to select all number buttons using querySelectorAll? 
-// document.querySelectorAll(".calculator__btn-num")
+// -----------------------------------------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------------------------------------
+
+/* UPDATE DISPLAY OUTPUT */
+// to display the output --> dynamically changing the inner html of calculator__display
+const updateDisplay = (number) => {
+  calcDisplay.innerHTML = number;
+};
+
+/* NUMBER BUTTON FUNCTIONALITY */
+
+// updating display -> for each calculator btn number -> listen out for the event.target -> update the inner html
+
+numberBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", (event) => {
+    initialValue += event.target.innerHTML;
+    updateDisplay(initialValue);
+  });
+});
+
+/* OPERATOR BUTTON FUNCTIONALITY */
+// for each operator btn -> listen out for click event to determine which element the event occurred upon
+operatorBtns.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    operator = event.target.value;
+    console.log(operator); /* this can be removed later */
+
+    // store the initialValue in a variable to be able to use in the sums below
+    // parseFloat -> contains a decimal place/floating point number
+    if (initialValue) {
+      storedValue = parseFloat(initialValue);
+      topDisplay.innerHTML = storedValue;
+      initialValue = "";
+      console.log(storedValue); /* this can be removed later */
+    }
+
+    if (operator.value === "+") {
+      // run the addValues function
+      sum = storedValue + parseFloat(initialValue);
+      //   addValues();
+    } else if (operator === "-") {
+      // run the subtractValues function
+      sum = storedValue - parseFloat(initialValue);
+    } else if (operator === "*") {
+      // run the multiplyValues function
+      sum = storedValue * parseFloat(initialValue);
+    } else if (operator === "/") {
+      // run the divideValues function
+      sum = storedValue / parseFloat(initialValue);
+      //   calcDisplay.innerHTML = storedValue / parseFloat(initialValue);
+    }
+    updateDisplay(sum);
+  });
+});
+
+/* EQUALS BUTTON FUNCTIONALITY */
+equalsBtn.addEventListener("click", (event) => {
+  console.log(event.target.value);
+});
+
+// const calculateTotal = () => {
+// add code here
+// }
 
 
+/* CLEAR BUTTON FUNTIONALITY */
+const clearDisplay = (event) => {
+  // reset values
+  initialValue = ""; /* does this need to be 0 instead of "" */
+  updateDisplay(initialValue);
+};
 
+clearBtn.addEventListener("click", clearDisplay);
 
-// Variables
+/* DECIMAL BUTTON FUNCTIONALITY */
+// stop user from adding more than one decimal point
 
+// OPERATOR FUNCTIONALITY
+// const operatorBtn = document.querySelectorAll(".calculator__btn")
+// if(operatorBtn === "+"){
+//     calcDisplay.innerHTML =
+// }
 
 //listen out for input 1
 //save the input?
 //listen out for the operator
 // listen out for the second input
-// equals will 
+// equals will
 //updating the output
 
-// Reusable functions
-// const clearDisplay = () => {
-//     calculator__output.innerHTML = 0;
-// }
+// const addValues = (initialValue, secondValue) => {
+//     calcDisplay.innerHTML = parseFloat(initialValue)
+//     + parseFloat(secondValue);
+//     // let sum;
+//     // sum = initialValue + secondValue;
+//     // calcDisplay.innerHTML = sum;
+// };
 
-// const addValues = () => {
-
-// }
-
-// You can also refer to an external "named" function:
-// element.addEventListener("click", myFunction);
-// function myFunction() {
-//   alert ("Hello World!");
-// }
-
-// Functions
-// Event listener - on click event
-calcBtnAdd.addEventListener("click", addValues);
-
-calcBtnClear.addEventListener("click", clearDisplay);
-// to display the output --> dynamically changing the inner html of calculator__output
-
-//const updateOutput = (number) => calculator__output.innerHTML = number;
-
-//The conditional operator assigns a value to a variable based on a condition.
-//variablename = (condition) ? value1:value2	voteable = (age < 18) ? "Too young":"Old enough";	
-
-// updating output
-// for each calculator btn number
-//listen out for the value
-//update the inner html
-
-//display number
-//on click of each button
-//display the value in the calculator output
-
-calcBtnThree.addEventListener("click",() {
-//
-});
-
-
-
-// document.getElementById(".calculator__output").innerHTML = "1234";
+// -----------------------------------------------------------------------------
+// EVENT LISTENERS
+// -----------------------------------------------------------------------------
