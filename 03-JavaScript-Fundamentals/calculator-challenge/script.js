bfe5852f3978e7a017ca4175022d3e143da5c35f
amiehannah <64609830+amiehannah@users.noreply.github.com>
2022-01-02 11:53:15 +0000

    tweaks to script file

--- 03-JavaScript-Fundamentals/calculator-challenge/script.js
+++ 03-JavaScript-Fundamentals/calculator-challenge/script.js
@@ -42,19 +42,20 @@ numberBtns.forEach((numBtn) => {
 operatorBtns.forEach((operatorBtn) => {
   operatorBtn.addEventListener("click", (event) => {
     operator = event.target.value;
-    // store the firstValue in a variable to be able to use in the sums below
-    // parseFloat -> contains a decimal place/floating point number
     if (firstValue) {
       /* once user clicks on a operator button move value in btmCalDisplay to topCalcDisplay */
       topCalcDisplay.innerHTML = firstValue + " " + operator;
       btmCalcDisplay.innerHTML = "";
+      // store the firstValue in a variable to be able to use in the sums below
+      // parseFloat -> contains a decimal place/floating point number
       storedValue = parseFloat(firstValue);
       firstValue = "";
 
+      /* need to work on this section -> it fails to show in the topCalcDisplay box at the moment*/
       if (sum) {
         /* when user clicks on an operator btn again move the sum value to topcaldisplay */
-      topCalcDisplay.innerHTML = storedValue;
-      btmCalcDisplay.innerHTML = "";
+        topCalcDisplay.innerHTML = storedValue;
+        btmCalcDisplay.innerHTML = "";
       }
     }
   });
@@ -82,7 +83,6 @@ const calculateTotal = () => {
 
 equalsBtn.addEventListener("click", calculateTotal);
 
-
 /* CLEAR BUTTON FUNTIONALITY */
 const clearDisplay = () => {
   // reset all values
@@ -96,10 +96,9 @@ const clearDisplay = () => {
 
 clearBtn.addEventListener("click", clearDisplay);
 
-
-
 /* HELP */
 // - [ ] How to stop user from adding more than one decimal point
+// - [ ] if user was to use a floating point number smaller than 1 - to add the 0 in front of it
 // - [ ] How to contiue calculations once equals has been pressed and visually move it into the right display box
 // - [ ] Target.event.value -> recap explanation
 
