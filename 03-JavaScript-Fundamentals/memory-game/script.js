f216f3347c71cfd9d0ecfcf0863e04dfcefbd3d3
amiehannah <64609830+amiehannah@users.noreply.github.com>
2022-01-12 11:54:32 +0000

    adding updates

--- 03-JavaScript-Fundamentals/memory-game/script.js
+++ 03-JavaScript-Fundamentals/memory-game/script.js
@@ -1,127 +1,18 @@
-// -----------------------------------------------------------------------------
-// SELECTING DOM ELEMENTS
-// -----------------------------------------------------------------------------
+//PSEUDOCODE
 
-// calculateMoves
-// increments by 1 every time user clicks on a card
-// updates the innerhtml of the calculateMovesCounter
-let moves = 0;
-const movesCounter = document.querySelector(".moves");
-
-// selecting timer counter
-let minutes = 0;
-let seconds = 0;
-const timerCounter = document.querySelector(".timer");
-
-// restart game button
-const restartGameBtn = document.querySelector(".restart");
-
-const cardSelected = document.querySelectorAll(".card");
-
-const gridOfCards = document.querySelector(".grid__container") /* DO I NEED TO SELECT THIS ELEMENT */
-
-// console.log(restartGameBtn);
-// console.log(timerCounter);
-// console.log(movesCounter);
-// console.log(gridOfCards);
-// console.log(cardSelected);
-
-// Need to store the images inside a data structure - do I use an array of arrays
-
-// store cards images in an array
-const cardsArr = [
-["./images/aries.svg"],
-["./images/taurus.svg"],
-["./images/gemini.svg"],
-["./images/cancer.svg"],
-["./images/leo.svg"],
-["./images/virgo.svg"],
-["./images/libra.svg"],
-["./images/scorpio.svg"],
-["./images/sagittarius.svg"],
-["./images/capricorn.svg"],
-["./images/aquarius.svg"],
-["./images/pisces.svg"],
-["./images/aries.svg"],
-["./images/taurus.svg"],
-["./images/gemini.svg"],
-["./images/cancer.svg"],
-["./images/leo.svg"],
-["./images/virgo.svg"],
-["./images/libra.svg"],
-["./images/scorpio.svg"],
-["./images/sagittarius.svg"],
-["./images/capricorn.svg"],
-["./images/aquarius.svg"],
-["./images/pisces.svg"]
-]
-
-
-
-// -----------------------------------------------------------------------------
-// FUNCTIONS
-// -----------------------------------------------------------------------------
-
-// at the start of the game cards values are hidden
-
-
-
-const calculateMoves = () => {
-moves++;
-movesCounter.innerHTML = `${moves} Move(s)`;
-    // movesCounter
-  // increments by 1 every time user clicks on a card
-  // updates the innerhtml of the MovesCounter
-  // does this need a forloop?
-};
-
-
-// cardSelected.addEventListener("click", calculateMoves());
-
-// timer function
-// keeps track of how long the user has been playing
-// once the user makes the first move, start the timer
-// increment second and minute timers
+// at the start of the game the cards values are hidden
+// game begins when user clicks on a card
+// movesCounter function is activated upon the first click and keep track of each move the user makes - Y
+// timer function is also activated upon the first click - Y
+// first card is selected, then second card is selected
+// these are compared to see if they match - if they match cards remain visible, if no match is found, they remain hidden until it's matching pair is found
+// FLIP ANIMATION
 // once all pairs have been matched, game ends and timer stops
-// do i display this in a pop up? or update the instruction section to say congrats / time elapsed / moves taken
+// to restart the game - user clicks on restart game button
+// this hides all the cards again
+// this shuffles the images 
+// this returns moveCounter and timer functions to zero so the game can start again - Y
 
-// restart game function
-const restartGame = () => {
-  // restart game
-  cardSelected.classList.add(".hidden");
-  // reset the board styling
-  // shuffles position of cards - using math.random?
-  // resets the timer and moves counters
-  // movesCounter.innerHTML = "0 Move(s)"
-  // timerCounter.innerHTML = "0 Mins 0 Secs"
-};
-
-
-// I want the card selected to be random
-const shuffleCard = () => {
-  const randomNumber = Math.floor(Math.random() * cardsArr.length);
-  // cardSelected.style.
-
-  //the element we want to change = [randomNumber][]
-  // cardSelected.style.backgroundImage = [randomNumber][0];
-  //minutes = 0;
-  //seconds = 0;
-}
-
-//   const getQuote = () => {
-//     const randomNumber = Math.floor( Math.random() * quotes.length )
-//     quoteText.innerHTML = quotes[randomNumber][0]
-
-//     background.style.backgroundImage = `url(${quotes[randomNumber][1]})`
-// }
-
-// MATH.RANDOM
-// Return a random number between 1 and 100:
-// Math.floor((Math.random() * 100) + 1);
-
-
-
-restartGameBtn.addEventListener("click", restartGame);
 
 // on clicking a card, need to change styling and reveal what is on the reverse
 // needs to check the subsequent card selected by the user
@@ -130,3 +21,283 @@ restartGameBtn.addEventListener("click", restartGame);
 // then card checking function runs again
 // checking first card then checking second card and repeat in this fashion
 // until all matching pairs have been found
+
+
+// -----------------------------------------------------------------------------
+// SELECTING DOM ELEMENTS
+// -----------------------------------------------------------------------------
+// selecting moves counter
+const movesCounter = document.querySelector(".moves");
+
+// selecting timer counter
+
+const timerCounter = document.querySelector(".timer");
+
+// restart game button
+const restartBtn = document.querySelector(".restart");
+
+// const gridContainer = document.querySelector(".grid__container")
+// do i need this?
+
+let moves = 0;
+let minutes = 0;
+let seconds = 0;
+
+
+
+// Need to store the images inside a data structure
+// store cards images in an array
+// or would it be better to put it in an object
+// const cardsArr = [
+//   "./images/aries.svg",
+//   "./images/taurus.svg",
+//   "./images/gemini.svg",
+//   "./images/cancer.svg",
+//   "./images/leo.svg",
+//   "./images/virgo.svg",
+//   "./images/libra.svg",
+//   "./images/scorpio.svg",
+//   "./images/sagittarius.svg",
+//   "./images/capricorn.svg",
+//   "./images/aquarius.svg",
+//   "./images/pisces.svg",
+//   "./images/aries.svg",
+//   "./images/taurus.svg",
+//   "./images/gemini.svg",
+//   "./images/cancer.svg",
+//   "./images/leo.svg",
+//   "./images/virgo.svg",
+//   "./images/libra.svg",
+//   "./images/scorpio.svg",
+//   "./images/sagittarius.svg",
+//   "./images/capricorn.svg",
+//   "./images/aquarius.svg",
+//   "./images/pisces.svg",
+// ];
+
+const cardsArr = [
+  {
+    name: "aries",
+    img: "images/aries.svg",
+  },
+  {
+    name: "aries",
+    img: "images/aries.svg",
+  },
+  {
+    name: "taurus",
+    img: "images/taurus.svg",
+  },
+  {
+    name: "taurus",
+    img: "images/taurus.svg",
+  },
+  {
+    name: "gemini",
+    img: "images/gemini.svg",
+  },
+  {
+    name: "gemini",
+    img: "images/gemini.svg",
+  },
+  { 
+    name: "cancer", 
+    img: "images/cancer.svg",
+  },
+  {
+    name: "cancer", 
+    img: "images/cancer.svg",
+  },
+  {
+    name: "leo", 
+    img: "images/leo.svg",
+  },
+  {
+    name: "leo", 
+    img: "images/leo.svg",
+  },
+   {
+    name: "virgo", 
+    img: "images/virgo.svg",
+   },
+   {
+    name: "virgo", 
+    img: "images/virgo.svg",
+   },
+   {
+    name: "libra", 
+    img: "images/libra.svg",
+   },
+   {
+    name: "libra", 
+    img: "images/libra.svg",
+   },
+   {
+    name: "scorpio", 
+    img: "images/scorpio.svg",
+   },
+   {
+    name: "scorpio", 
+    img: "images/scorpio.svg",
+   },
+   {
+    name: "sagittarius", 
+    img: "images/sagittarius.svg",
+   },
+   {
+    name: "sagittarius", 
+    img: "images/sagittarius.svg",
+   },
+   {
+    name: "capricorn", 
+    img: "images/capricorn.svg",
+   },
+   {
+    name: "capricorn", 
+    img: "images/capricorn.svg",
+   },
+   {
+    name: "aquarius", 
+    img: "images/aquarius.svg",
+   },
+   {
+    name: "aquarius", 
+    img: "images/aquarius.svg",
+   },
+   {
+     name: "pisces",
+     img: "images/pisces.svg",
+   },
+   {
+    name: "pisces",
+    img: "images/pisces.svg",
+  }
+];
+
+// console.log(cardsArr.length)
+
+// -----------------------------------------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------------------------------------
+
+
+
+const cards = document.querySelectorAll('.card') //produces a nodelist
+
+
+cards.forEach(card => {
+  card.addEventListener('click', clickOutcome);
+})
+
+function clickOutcome() {
+  // alert('I have been clicked!')
+  calculateMoves();
+}
+
+// SHUFFLE CARD FUNCTION
+
+for(let i=0; i< cardsArr.length; i++) {
+  console.log(cardsArr[i]) //print each card to the console [i] every index;
+  // cardsArr[i].style = "display: none";
+  const randomNumber = Math.floor(Math.random() * cardsArr.length);
+  console.log(randomNumber)
+  cardsArr[i] = [randomNumber];
+  // append each zodiac element to the grid
+
+
+}
+
+
+
+// SHUFFLE CARDS
+// I want the card selected to be random
+// generate random number
+// assign cardsArr a new random index
+// move the cards in a random fashion on the board
+//
+// const shuffleCards = () => {
+//   const randomNumber = Math.floor(Math.random() * cardsArr.length);
+
+// }
+
+//   cards.style.backgroundImage = `url(${cardsArr[randomNumber][1]})`
+//   //the element we want to change = [randomNumber][]
+//   // cardSelected.style.backgroundImage = [randomNumber][0];
+// };
+// shuffleCards();
+
+
+
+
+
+// CALCULATE MOVES FUNCTION
+// this tracks the moves the user takes and starts the timer
+// increments by 1 every time user clicks on a card
+// updates the innerhtml of the calculateMoves Counter
+
+const calculateMoves = () => {
+  moves++;
+  movesCounter.innerHTML = `${moves} Move(s)`;
+  if (moves == 1) {
+    seconds = 0;
+    minutes = 0;
+    startTimer();
+  }
+};
+
+
+// START TIMER FUNCTION
+// timer function
+// keeps track of how long the user has been playing
+// once the user makes the first move, start the timer
+// increment second and minute timers
+// once all pairs have been matched, game ends and timer stops
+// do i display this in a pop up? or update the instruction section to say congrats / time elapsed / moves taken
+
+/* FIRST ATTEMPT */
+let timerId;
+const startTimer = () => {
+  timerId = setInterval(function () {
+    timerCounter.innerHTML = `${minutes} Mins ${seconds} Secs`;
+    seconds++;
+    if (seconds == 60) {
+      minutes++;
+      seconds = 0;
+    }
+  }, 1000);
+};
+
+
+// STOP/RESET TIMER
+// this needs to be invoked on both matching all cards - stop timer
+// and invoked when user clicks 'Reset Game' btn
+
+const resetTimer = () => {
+  timerCounter.innerHTML = "0 Mins 0 Secs";
+  seconds = 0;
+  minutes = 0;
+  moves = 0;
+  movesCounter.innerHTML = "0 Move(s)";
+  clearInterval(timerId);
+
+// reset the board styling
+// shuffles position of cards - using math.random?
+};
+
+restartBtn.addEventListener("click", resetTimer);
+
+
+
+
+
+// cardsArr[0].classList.toggle(".hidden")
+
+
+
+// cards.addEventListener('click', () => {
+//   if(cards.style.display === 'none') {
+//     cards.style.display = 'block';
+// } else {
+// cards.style.display = 'none';
+// }
+// });
