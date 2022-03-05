2d46482e1b2b179ab799a2c4265b514a3d1fc6a2
amiehannah <64609830+amiehannah@users.noreply.github.com>
2022-03-05 18:37:07 +0000

    finalised styling

---
+++ 03-JavaScript-Fundamentals/morse-code-translator/README.md
@@ -0,0 +1,13 @@
+Task:
+To create a morse code translator using OOP.
+The classes used should store all the information needed to translate between Morse code and English (and back again) including alphabets, the word to translate and the eventual output.
+
+Breaking down the problem --> Essential functionality needed:
+• Data structure to store morse code characters in JS 
+• 
+
+
+● Build a Class of Translator – this will be extended multiple times for lots of different types of translators.
+● Build 2 further Classes (Morse & French/Spanish/German etc) that extend from the translator class and inherit any properties/methods you think it should have.
+● Build these translators out to be fully functioning.
+● The definition of done is when I (the user) can visit you hosted site, input something in English and receive back the equivalent in, for example, Morse Code.
--- 03-JavaScript-Fundamentals/morse-code-translator/script.js
+++ 03-JavaScript-Fundamentals/morse-code-translator/script.js
@@ -1,29 +1,73 @@
+/*
+
+1. Set up event listeners 
+2. Build an object class containing morse code letters, in Object key:value pairs
+3. Able to translate word/s (parameter to pass in)
+4. Take word/input -> break it up into an array
+5. Format morse code -> contains spaces between each letter and a slash / to separate words
+6. Loop through array to convert characters
+
+*/
+
+
+const userInput = document.querySelector(".input__text");
+const outputDisplay = document.querySelector(".output__text");
+const clearButton = document.querySelector(".clear__button");
+const translateButton = document.querySelector(".translate__button");
+
+
+
 // STORE MORSE CODE CHARACTERS IN OBJECT KEY:VALUE PAIRS
-const morseCharacters = {
-    a : "• -" ,
-    b : "- • • •" ,
-    c : "- • - •",
-    d : "- • •",
-    e : "•",
-    f : "• • - •",
-    g : "- - •",
-    h : "• • • •",
-    i : "• •",
-    j : "• - - -",
-    k : "- • -",
-    l : "• - • •",
-    m : "- -",
-    n : "- •",
-    o : "- - -",
-    p : "• - - •",
-    q : "- - • -",
-    r : "• - •",
-    s : "• • •",
-    t : "-",
-    u : "• • -",
-    v : "• • • -",
-    w : "• - -",
-    x : "- • • -",
-    y : "- • - -",
-    z : "- - • •"
+class EnglishToMorseCodeChars {
+    constructor() {
+       this.a = "• -" ,
+        this.b = "- • • •" ,
+        this.c = "- • - •",
+        this.d = "- • •",
+        this.e = "•",
+        this.f = "• • - •",
+        this.g = "- - •",
+        this.h = "• • • •",
+        this.i = "• •",
+        this.j = "• - - -",
+        this.k = "- • -",
+        this.l = "• - • •",
+        this.m = "- -",
+        this.n = "- •",
+        this.o = "- - -",
+        this.p = "• - - •",
+        this.q = "- - • -",
+        this.r = "• - •",
+        this.s = "• • •",
+        this.t = "-",
+        this.u = "• • -",
+        this.v = "• • • -",
+        this.w = "• - -",
+        this.x = "- • • -",
+        this.y = "- • - -",
+        this.z = "- - • •"
+    }
 }
+
+// BUILD A CLASS OF TRANSLATOR
+
+
+
+// GET INPUT FROM THE USER
+const displayTranslation = () => {
+    const usersInputValue  = document.querySelector(".input__text").value;
+    outputDisplay.innerHTML = usersInputValue;
+}
+
+translateButton.addEventListener("click", displayTranslation);
+
+
+
+// CLEAR THE USER INPUT
+const resetTranslator = () => {
+    userInput.value = "";
+    outputDisplay.innerHTML = "";
+}
+
+clearButton.addEventListener("click", resetTranslator);
+
