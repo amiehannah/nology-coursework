8647bf9c3740b957668771ae618286b45288cf25
amiehannah <64609830+amiehannah@users.noreply.github.com>
2022-03-28 20:48:26 +0100

    refactored code

--- 03-JavaScript-Fundamentals/hostile-aliens-game/script.js
+++ 03-JavaScript-Fundamentals/hostile-aliens-game/script.js
@@ -4,126 +4,132 @@ const defenceShipScore = document.querySelector("#d-ship-score");
 const attackShipScore = document.querySelector("#a-ship-score");
 const startButton = document.querySelector(".start-button");
 const modal = document.querySelector("#modal");
-const gameOver = document.querySelector(".game-over");
+const gameOverModal = document.querySelector(".game-over");
 const restartGame = document.querySelector(".restart-button");
 
- gameOver.classList.add("hidden"); // DO SOMETHING WITH THIS LATER
+const gameContainer = document.querySelector(".gameContainer");
+
+modal.classList.add("hidden")
+gameOverModal.classList.add("hidden"); 
+// DO SOMETHING WITH THIS LATER
 // restartGame.addEventListener('click', () => {
-  
 // })
 
-// motherShipScore.innerHTML = "20";
-// defenceShipScore.innerHTML = "20";
-// attackShipScore.innerHTML = "20";
-
-/* 
-// ON STARTING THE GAME RANDOMISE THE SEQUENCE OF SHIPS
-// ON USER CLICKING THE BUTTON -> GENERATE A RANDOM NUMBER
-// USING THE RANDOM NUMBER GENERATED FIND THE SHIP AT THAT RANDOM NUMBER INDEX
-// CALL THE METHOD - shipIsHit() ON EACH BUTTON CLICK, RANDOM ALIEN SHIP IS HIT AND POINTS ARE REDUCED
-// REDUCE THE SCORE VISUALLY ON SCORE BOARD + ONCE HIT ADD A CSS EFFECT
-// ONCE THE ALIEN SHIPS POINT HAVE HIT ZERO THE SHIP IS DESTROYED AND CAN'T BE HIT AGAIN
-// STORE HIT SHIPS IN A NEW ARRAY (PUSH IT)? ADD CLASS LIST OF HIDDEN?
-// GAME OVER WHEN ALL ALIEN SHIPS HAVE BEEN HIT or MOTHERSHIP = 0
-// GAME OVER - GAME RESTARTS
-
-*/
-// TO DO:
-// HOW TO CREATE THE ALIEN FLEET EASILY - IS THERE A BETTER WAY OF DOING THIS?
-// HOW DO YOU LINK THE ARRAY TO THE HTML EQUIVALENTS?
-// ONCE HIT ADD CSS EFFECT - BLINK OR CHANGE COLOUR
-// ONCE HIT REDUCE TOTAL SCORE (NEED TO RELOOK AT MY SCORE TABLE)
-// RANDOMISE ARRAY ON GAME LOAD
-// ADD GAME OVER MODAL
-
 class alienShip {
-  constructor(shipName, currentPoints, hitPoints) {
+  constructor(shipName, currentPoints, hitPoints, id) {
     this.shipName = shipName;
     this.currentPoints = currentPoints;
     this.hitPoints = hitPoints;
+    this.id = id;
   }
   getCurrentScore() {
       return this.currentPoints;
   }
-
-  shipIsHit() {
-   // when button is pressed random alien ship is hit
-   // points are reduced
+  reduceShipPoints() {  // points are reduced
    const alienShipScore = this.currentPoints - this.hitPoints;
    this.currentPoints = alienShipScore;
    return this.currentPoints;
   }
-
   gameOver() {
       this.currentPoints = 0;
+      gameOverModal.classList.remove("hidden");
   }
-} // end of class
+  createShip() {
+    return `<div class=${this.shipName} id="${this.id}>${this.shipName}</div>`
+  }
+} 
 
-
-
+//CREATE ARRAY OF SHIPS
+//LOOP THROUGH AND CREATE X AMOUNT OF EACH SHIP FOR THE BOARD
+const shipsArr = [];
 const createAlienFleet = () => {
-    alienMotherShip = new alienShip("motherShip", 100, 9);
-    alienDefenceShip = new alienShip("defenceShip", 80, 10);
-    alienDefenceShip2 = new alienShip("defenceShip", 80, 10);
-    alienDefenceShip3 = new alienShip("defenceShip", 80, 10);
-    alienDefenceShip4 = new alienShip("defenceShip", 80, 10);
-    alienDefenceShip5 = new alienShip("defenceShip", 80, 10);
-    alienAttackShip = new alienShip("attackShip", 45, 12);
-    alienAttackShip2 = new alienShip("attackShip", 45, 12);
-    alienAttackShip3 = new alienShip("attackShip", 45, 12);
-    alienAttackShip4 = new alienShip("attackShip", 45, 12);
-    alienAttackShip5 = new alienShip("attackShip", 45, 12);
-    alienAttackShip6 = new alienShip("attackShip", 45, 12);
-    alienAttackShip7 = new alienShip("attackShip", 45, 12);
-    alienAttackShip8 = new alienShip("attackShip", 45, 12);
+    alienMotherShip = new alienShip("motherShip", 100, 9, 1);
+    shipsArr.push(alienMotherShip);
+    for(let i = 0; i < 5; i++) {
+      alienDefenceShip = new alienShip("defenceShip", 80, 10, i + 2);
+      shipsArr.push(alienDefenceShip);
+    }
+    for(let i=0; i < 8; i++) {
+      alienAttackShip = new alienShip("attackShip", 45, 12, i + 7);
+      // console.log(alienAttackShip);
+      shipsArr.push(alienAttackShip);
+      // console.log(shipsArr);
+    }
 }
 
+
 createAlienFleet();
+// console.log(alienMotherShip.createShip());
 
-console.log(createAlienFleet);
+const renderShip = () => {
+  shipsArr.map(ship => {
+    // console.log(ship);
+    // console.log(ship.createShip())
+    return `<div class=${this.shipName} id="${this.id}>${this.shipName}</div>`
+    // gameContainer.innerHTML += ship.createShip();
+    // gameContainer.appendChild(`${ship}.createShip()`);
+  })
+}
+renderShip();
 
-console.log(alienAttackShip2.getCurrentScore());
-console.log(alienAttackShip2.shipIsHit()); //shipIsHit method works
-
-
-
-
-// START GAME CLICK HIDES MODAL
+// START GAME CLICK HIDES MODAL (& SHUFFLES ARRAY?)
 startButton.addEventListener('click', () => {
   modal.classList.add("hidden");
-
-// ON LOAD DO I NEED TO RANDOMISE ARRAY EACH TIME?
-// loadGame(); function?
 })
 
-// STORE ALIEN SHIPS IN ARRAY
 
-let alienShipFleetArr = [];
-alienShipFleetArr
-.push(
-  alienMotherShip,
-  alienDefenceShip,
-  alienDefenceShip2,
-  alienDefenceShip3,
-  alienDefenceShip4,
-  alienDefenceShip5,
-  alienAttackShip,
-  alienAttackShip2,
-  alienAttackShip3,
-  alienAttackShip4,
-  alienAttackShip5,
-  alienAttackShip6,
-  alienAttackShip7,
-  alienAttackShip8
-);
-console.log(alienShipFleetArr);
+// SHUFFLE ARRAY AT GAME START
+// ON STARTING THE GAME RANDOMISE THE SEQUENCE OF SHIPS
+const shuffleArray = shipsArr.sort(() => Math.random() - 0.5);
+// console.log(shuffleArray); //TO CHECK IT'S SHUFFLED OK
 
 
 
-shootButton.addEventListener("click", () => {
-  let randomShip = alienShipFleetArr[Math.floor(Math.random()*alienShipFleetArr.length)];
+
+
+
+
+// ON USER CLICKING THE BUTTON -> GENERATE A RANDOM NUMBER
+// USING THE RANDOM NUMBER GENERATED FIND THE SHIP AT THAT RANDOM NUMBER INDEX
+const shootAlienShip = () => {
+  let randomShip = shuffleArray[Math.floor(Math.random()*shuffleArray.length)];
   console.log(randomShip);
-  randomShip.shipIsHit();
-  return randomShip;
-  })
+  console.log(randomShip.shipName);
+
+  if(alienMotherShip.currentPoints === 0 || (alienDefenceShip.currentPoints === 0 && alienAttackShip.currentPoints === 0)){
+    randomShip.gameOver();
+  } else {
+    randomShip.reduceShipPoints();
+  }
   
+    if (randomShip === alienMotherShip) {
+      motherShipScore.innerHTML = alienMotherShip.getCurrentScore();
+    } else {
+      if (randomShip.shipName === alienDefenceShip) {
+     
+        defenceShipScore.innerHTML = alienDefenceShip.getCurrentScore();
+      }
+    }
+      
+  if(randomShip.currentPoints <=0) {
+  hideAlienShip();
+  }
+}
+
+
+
+
+const hideAlienShip = () => {
+
+}
+
+
+//EVENT LISTENERS
+shootButton.addEventListener("click", () => {
+  shootAlienShip();
+})
+restartGame.addEventListener("click", () => {
+gameOverModal.classList.add("hidden");
+})
+
+
